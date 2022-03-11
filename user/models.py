1df91260c44837bb5904c807202e2b5a8bc4c6c4
Mahammad <mahammad.nabizade@gmail.com>
2022-03-11 19:17:30 +0400

    models are created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
+from itertools import count, product
 from django.db import models
 
 # Create your models here.
+
+
+class WishList(models.Model):
+    product_id = models.IntegerField('product id')
+    user_id = models.IntegerField("user id")
+
+
+class Basket(models.Model):
+    user_id = models.IntegerField('basket id') 
+
+class BasketItems(models.Model):
+    price = models.IntegerField('price')
+    product_id = models.IntegerField
+    basket_id = models.IntegerField
+    count = models.IntegerField("number of items")
+
